e2e057ce8c1a36d909edd51630b5e6628ccc6a00
benjamin meneses <benjamin.meneses.14@sansano.usm.cl>
2019-05-02 18:18:33 -0400

    AGH

--- modelo/arduino.js
+++ modelo/arduino.js
@@ -2,11 +2,5 @@ const SerialPort = require('serialport');
 const Readline = require('@serialport/parser-readline');
 const finalPort = new SerialPort('/dev/ttyACM0', { baudRate: 9600 });
 const parser = finalPort.pipe(new Readline({ delimiter: '\n' }));
-// Read the port data
-parser.on("open", () => {
-    $("#footerdiv").html("puerto serial abierto");
-});
 
-parser.on('data', data =>{
-    $("#maindiv").html(data);
-});
+module.exports = parser;
