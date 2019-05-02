d89a8919638fbfd6fcebc55f393ccbfa9591ce2c
benjamin meneses <benjamin.meneses.14@sansano.usm.cl>
2019-05-02 18:14:12 -0400

    cambios?

--- modelo/arduino.js
+++ modelo/arduino.js
@@ -1,18 +1,12 @@
 const SerialPort = require('serialport');
 const Readline = require('@serialport/parser-readline');
-const parser = port.pipe(new Readline({ delimiter: '\n' }));
+const finalPort = new SerialPort('/dev/ttyACM0', { baudRate: 9600 });
+const parser = finalPort.pipe(new Readline({ delimiter: '\n' }));
+// Read the port data
+parser.on("open", () => {
+    $("#footerdiv").html("puerto serial abierto");
+});
 
-function setConnection(port){
-    if(port == null){
-        port = '/dev/ttyACM0';
-    };
-    const finalPort = new SerialPort(port, { baudRate: 9600 });
-    // Read the port data
-    finalPort.on("open", () => {
-        $("#footerdiv").html("puerto serial abierto");
-    });
-
-    finalPort.on('data', data =>{
-        $("#maindiv").html(data);
-    });
-}
+parser.on('data', data =>{
+    $("#maindiv").html(data);
+});
